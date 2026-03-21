27f7256ee0db59e55f8f2de1fb0171230fb62a65
hamza4898 <hamlipochta2@gmail.com>
2026-03-21 12:41:29 +0300

    feat(frontend): add frontend implementation for the new JWT login

---
+++ Frontend/Login.js
@@ -0,0 +1,27 @@
+async function loginUser(formId) {
+    const form = document.getElementById(formId);
+    const data = Object.fromEntries(new FormData(form));
+
+    try {
+        const response = await fetch('/api/login/LoginUser', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+
+        if (response.ok) {
+            window.location.href = "/Profile.html";
+        } else {
+            const errorData = await response.json();
+            
+            if (errorData && errorData.error) {
+                const messages = errorData.error;
+                alert("Ошибки:\n" + messages);
+            } else {
+                alert("Ошибка сервера: " + response.status);
+            }
+        }
+    } catch (err) {
+        console.error("Ошибка запроса:", err);
+    }
+}
